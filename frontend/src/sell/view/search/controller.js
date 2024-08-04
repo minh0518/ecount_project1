f294b7609a5047d8f33275bee4c54c90aacde1c8
minh0518 <rubics@naver.com>
2024-08-04 13:41:25 +0900

    주석 수정

--- frontend/src/sell/view/search/controller.js
+++ frontend/src/sell/view/search/controller.js
@@ -2,12 +2,12 @@ import { item_data } from '../../data/data.js';
 import { events } from './event.js';
 import { getSearchDataApi } from './service/get.js';
 
-// 현재 페이지 상태
+// 상태
 let currentPage = 1;
 
-// 현재 페이지 상태 업데이트 및 리렌더링
+// 상태 업데이트(리렌더링 포함)
 function setCurrentPage(target, pageCount, tbody) {
-  // 리렌더링
+  // 상태 업데이트
   currentPage += target;
   if (currentPage > pageCount) currentPage = pageCount;
   if (currentPage <= 0) currentPage = 1;
@@ -20,11 +20,10 @@ export const render = (page, tbody) => {
   const startOffset = (page - 1) * 10;
   const endOffset = startOffset + 10;
 
-  // offset에 맞는 데이터 요청
-  const pageData = getSearchDataApi.getSearchList(startOffset, endOffset);
+  const currentPageData = getSearchDataApi.getSearchList(startOffset, endOffset);
 
   tbody.innerHTML = '';
-  for (let { code, name } of pageData) {
+  for (let { code, name } of currentPageData) {
     const newTr = document.createElement('tr');
     const htmlSting = `
                 <td><input type="checkbox"/></td>
@@ -44,47 +43,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const goLeftButton = controlButtons[0];
   const goRightButton = controlButtons[1];
 
-  // # 1페이지 테이블 세팅
-  render(currentPage, tbody);
-  // render(1, tbody);
-
-  // # 페이지네이션 버튼
-  // 데이터 전체 길이
   const totalCount = item_data.length;
-
-  // 전체 페이지 개수
   const pageCount = totalCount % 10 === 0 ? Math.floor(totalCount / 10) : Math.floor(totalCount / 10) + 1;
 
-  // 현재 페이지의 행 추가
+  // 첫페이지 테이블 세팅
+  render(currentPage, tbody);
+
+  // 페이지네이션 버튼 이벤트
   events.pageButtons(tbody, pageCount, pageButtons);
 
-  // # 이전, 다음 버튼 추가
+  // 이전, 다음 버튼 이벤트
   events.moveButton(goLeftButton, () => setCurrentPage(-1, pageCount, tbody));
   events.moveButton(goRightButton, () => setCurrentPage(1, pageCount, tbody));
 });
-
-// for (let { code, name } of item_data) {
-//   const newTr = document.createElement('tr');
-
-//   const checkboxTd = document.createElement('td');
-//   const checkbox = document.createElement('input');
-//   checkbox.type = 'checkbox';
-//   checkboxTd.appendChild(checkbox);
-//   newTr.appendChild(checkboxTd);
-
-//   const codeTd = document.createElement('td');
-//   codeTd.textContent = code;
-//   newTr.appendChild(codeTd);
-
-//   const nameTd = document.createElement('td');
-//   nameTd.textContent = name;
-//   newTr.appendChild(nameTd);
-
-//   const editTd = document.createElement('td');
-//   const button = document.createElement('button');
-//   button.innerText = '수정';
-//   editTd.appendChild(button);
-//   newTr.appendChild(editTd);
-
-//   tbody.appendChild(newTr);
-// }
--- frontend/src/sell/view/search/event.js
+++ frontend/src/sell/view/search/event.js
@@ -2,8 +2,6 @@ import { render } from './controller.js';
 
 export const events = {
   pageButtons: (tbody, pageCount, pageButtons) => {
-    // 페이지네이션 버튼 이벤트 등록
-
     // 26
     // 1페이지 : slice index 0~9
     // 2페이지 : slice index 10~19
@@ -16,8 +14,8 @@ export const events = {
       const singlePageButton = document.createElement('button');
       singlePageButton.innerText = currentPage;
 
-      // 이벤트 리스너가 등록될 당시의 변수를 계속해서 사용하는건 맞음
-      // 그러나 클로저에 의해 그 내용이 바뀔 수 있음
+      // 이벤트 리스너가 등록될 당시의 변수를 계속해서 사용하는건 맞지만
+      // 클로저에 의해 그 내용이 바뀔 수 있다
       singlePageButton.addEventListener('click', () => {
         render(page, tbody);
       });

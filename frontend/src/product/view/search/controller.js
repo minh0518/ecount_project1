1da9b111aab7f1d9c4b89d9b7d7801834448a912
minh0518 <rubics@naver.com>
2024-08-07 22:49:31 +0900

    판매 조회페이지 선택된 품목 제거 기능 추가

--- frontend/src/product/view/search/controller.js
+++ frontend/src/product/view/search/controller.js
@@ -1,8 +1,8 @@
-import { product_item_data } from "../../data/data.js";
-import { getSearchDataApi } from "../../service/get.js";
+import { product_item_data } from '../../data/data.js';
+import { getSearchDataApi } from '../../service/get.js';
 
 const params = new URLSearchParams(window.location.search);
-const from = params.get("from");
+const from = params.get('from');
 // const product_code = params.get("code");
 
 // TODO: 필요한 기능인지 확인
@@ -16,38 +16,38 @@ let totalCount = 1;
 export const renderTable = (currentPage) => {
   const startOffset = (currentPage - 1) * 10;
   const endOffset = startOffset + 10;
-  const nameInput = document.querySelector(".nameInput").value;
-  const codeInputArr = document.querySelector(".codeInput").value;
+  const nameInput = document.querySelector('.nameInput').value;
+  const codeInputArr = document.querySelector('.codeInput').value;
 
   const { data: currentPageStateData, length } = getSearchDataApi.getData(
     startOffset,
     endOffset,
     codeInputArr,
-    nameInput
+    nameInput,
   );
 
   totalCount = length; // 상태 업데이트
 
-  const tbody = document.querySelector(".tbody");
+  const tbody = document.querySelector('.tbody');
 
-  tbody.innerHTML = "";
+  tbody.innerHTML = '';
   for (let { code, name } of currentPageStateData) {
-    const newTr = document.createElement("tr");
+    const newTr = document.createElement('tr');
     const htmlSting = `
                 <td><input type="checkbox" class="checkbox"/></td>
                 <td><button class="codeButton">${code}</button></td>
-                <td id="product_${name}">${name}</td>
+                <td>${name}</td>
                 <td><button class="editButton">수정</button></td>
               `;
     newTr.innerHTML = htmlSting;
     tbody.appendChild(newTr);
 
-    const checkbox = newTr.querySelector(".checkbox");
+    const checkbox = newTr.querySelector('.checkbox');
 
-    checkbox.addEventListener("change", () => {
-      const allCheckboxes = document.querySelectorAll(".checkbox");
+    checkbox.addEventListener('change', () => {
+      const allCheckboxes = document.querySelectorAll('.checkbox');
       // 판매 입력(1개만 선택)
-      if (from === "add") {
+      if (from === 'add') {
         allCheckboxes.forEach((i) => {
           if (i.checked) i.checked = false;
         });
@@ -55,38 +55,31 @@ export const renderTable = (currentPage) => {
       }
 
       // 판매 조회(3개까지)
-      if (from === "search") {
+      if (from === 'search') {
         let count = 0;
         allCheckboxes.forEach((i) => {
           if (i.checked) count += 1;
         });
 
         if (count > 3) {
-          alert("최대 3개까지 선택 가능합니다");
+          alert('최대 3개까지 선택 가능합니다');
           checkbox.checked = false;
           return;
         }
       }
     });
 
-    const editButton = newTr.querySelector(".editButton");
-    editButton.addEventListener("click", () => {
-      window.open(
-        `../add/productAdd.html?mode=edit&code=${code}&name=${name}`,
-        "_blank",
-        "width=500,height=300"
-      );
+    const editButton = newTr.querySelector('.editButton');
+    editButton.addEventListener('click', () => {
+      window.open(`../add/productAdd.html?mode=edit&code=${code}&name=${name}`, '_blank', 'width=500,height=300');
     });
   }
 };
 
 // 메인로직
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   if (!localStorage.getItem(`product_item_data`)) {
-    localStorage.setItem(
-      `product_item_data`,
-      JSON.stringify(product_item_data)
-    );
+    localStorage.setItem(`product_item_data`, JSON.stringify(product_item_data));
   }
 
   // document.querySelector(".codeInput").value = codeQueryArr.length
@@ -95,70 +88,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
   renderTable(currentPage);
 
-  const leftButton = document.querySelector(".leftButton");
-  const rightButton = document.querySelector(".rightButton");
+  const leftButton = document.querySelector('.leftButton');
+  const rightButton = document.querySelector('.rightButton');
 
-  leftButton.addEventListener("click", () => {
+  leftButton.addEventListener('click', () => {
     if (currentPage > 1) {
       currentPage -= 1; // 상태 업데이트
       renderTable(currentPage);
     }
   });
 
-  rightButton.addEventListener("click", () => {
-    const totalPageCount =
-      totalCount % 10 === 0
-        ? Math.floor(totalCount / 10)
-        : Math.floor(totalCount / 10) + 1;
+  rightButton.addEventListener('click', () => {
+    const totalPageCount = totalCount % 10 === 0 ? Math.floor(totalCount / 10) : Math.floor(totalCount / 10) + 1;
     if (currentPage < totalPageCount) {
       currentPage += 1; // 상태 업데이트
       renderTable(currentPage);
     }
   });
 
-  const searchButton = document.querySelector(".searchButton");
-  searchButton.addEventListener("click", () => {
+  const searchButton = document.querySelector('.searchButton');
+  searchButton.addEventListener('click', () => {
     currentPage = 1;
     renderTable(currentPage);
   });
 
-  const applyButton = document.querySelector(".apply");
-  applyButton.addEventListener("click", () => {
+  const applyButton = document.querySelector('.apply');
+  applyButton.addEventListener('click', () => {
     // 판매 입력(1개만 선택)
-    if (from === "add") {
-      const allCheckboxes = document.querySelectorAll(".checkbox");
+    if (from === 'add') {
+      const allCheckboxes = document.querySelectorAll('.checkbox');
       for (let row of allCheckboxes) {
         if (row.checked) {
-          const targetTr = row.closest("tr");
-          const codeValue = targetTr.querySelectorAll("td")[1].innerText;
-          const nameValue = targetTr.querySelectorAll("td")[2].innerText;
-          opener.document.querySelector(
-            ".codeInput"
-          ).value = `${codeValue},${nameValue}`;
+          const targetTr = row.closest('tr');
+          const codeValue = targetTr.querySelectorAll('td')[1].innerText;
+          const nameValue = targetTr.querySelectorAll('td')[2].innerText;
+          opener.document.querySelector('.codeInput').value = `${codeValue},${nameValue}`;
           window.close();
           break;
         }
       }
     }
     // 판매 조회(3개까지)
-    if (from === "search") {
-      const allCheckboxes = document.querySelectorAll(".checkbox");
+    if (from === 'search') {
+      const parent = window.opener.document.querySelector('.codeList');
+      parent.innerHTML = '';
+      const allCheckboxes = document.querySelectorAll('.checkbox');
       allCheckboxes.forEach((row) => {
         if (row.checked) {
-          const targetTr = row.closest("tr");
-          const codeValue = targetTr.querySelectorAll("td")[1].innerText;
-          const nameValue = targetTr.querySelectorAll("td")[2].innerText;
+          const targetTr = row.closest('tr');
+          const codeValue = targetTr.querySelectorAll('td')[1].innerText;
+          const nameValue = targetTr.querySelectorAll('td')[2].innerText;
 
-          const dataDiv = document.createElement("div");
-          dataDiv.classList.add("singleSearchedData");
-          dataDiv.innerHTML = `<span id=${codeValue}>${codeValue}</span> <span>${nameValue}</span> <button class="deletesingleSearchedData">x</button>`;
-          dataDiv
-            .querySelector(".deletesingleSearchedData")
-            .addEventListener("click", () => {
-              dataDiv.style.display = "none";
-            });
-
-          const parent = window.opener.document.querySelector(".codeList");
+          const dataDiv = document.createElement('div');
+          dataDiv.classList.add('singleSearchedData');
+          dataDiv.innerHTML = `<span>${codeValue}</span> <span>${nameValue}</span> <button class="deleteSearchedData">x</button>`;
 
           parent.appendChild(dataDiv);
         }
@@ -167,12 +150,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const newButton = document.querySelector(".new");
-  newButton.addEventListener("click", () => {
-    window.open(
-      `../add/productAdd.html?mode=new`,
-      "_blank",
-      "width=500,height=300"
-    );
+  const newButton = document.querySelector('.new');
+  newButton.addEventListener('click', () => {
+    window.open(`../add/productAdd.html?mode=new`, '_blank', 'width=500,height=300');
   });
 });
--- frontend/src/sell/view/search/controller.js
+++ frontend/src/sell/view/search/controller.js
@@ -1,7 +1,7 @@
-import { sell_item_data } from "../../data/data.js";
-import { getSellDataApi } from "../../service/get.js";
-import { deleteSellDataApi } from "../../service/delete.js";
-import { getDateStringByDate } from "../../../utils/date.js";
+import { sell_item_data } from '../../data/data.js';
+import { getSellDataApi } from '../../service/get.js';
+import { deleteSellDataApi } from '../../service/delete.js';
+import { getDateStringByDate } from '../../../utils/date.js';
 
 // 상태
 let currentPage = 1;
@@ -19,62 +19,46 @@ let totalCount = 1;
 
 // 테이블 렌더링
 export const renderTable = (currentPage) => {
-
-  const startDate = document.getElementById('startDate').value
-  const endDate = document.getElementById('endDate').value
+  const startDate = document.getElementById('startDate').value;
+  const endDate = document.getElementById('endDate').value;
 
   const descriptionInput = document.querySelector(`.descriptionInput`).value;
-  const codeList = document.querySelector(".codeList");
+  const codeList = document.querySelector('.codeList');
 
   // codeList 내부의 모든 singleSearchedData 요소를 선택
-  const dataItems = codeList.querySelectorAll(".singleSearchedData");
+  const dataItems = codeList.querySelectorAll('.singleSearchedData');
   // 결과를 저장할 배열
   const codeResult = [];
   // 각 singleSearchedData 요소를 순회
   dataItems.forEach((item) => {
-    const firstSpan = item.querySelector("span");
+    const firstSpan = item.querySelector('span');
     if (firstSpan) {
       codeResult.push(firstSpan.innerText);
     }
   });
 
-
-
   const startOffset = (currentPage - 1) * 10;
   const endOffset = startOffset + 10;
-
 
   const { data: currentPageStateData, length } = getSellDataApi.getDataList(
     startOffset,
     endOffset,
     startDate === '' ? '' : new Date(startDate),
     endDate === '' ? '' : new Date(endDate),
-    codeResult
+    codeResult,
   );
-
 
   totalCount = length; // 상태 업데이트
 
-  const tbody = document.querySelector(".tbody");
+  const tbody = document.querySelector('.tbody');
 
-  tbody.innerHTML = "";
+  tbody.innerHTML = '';
 
-  for (let {
-    date,
-    code,
-    number,
-    name,
-    quantity,
-    price,
-    description,
-  } of currentPageStateData) {
-
-
-    const newTr = document.createElement("tr");
+  for (let { date, code, number, name, quantity, price, description } of currentPageStateData) {
+    const newTr = document.createElement('tr');
     const htmlSting = `
                 <td><input type="checkbox" class="checkbox"/></td>
-                <td><button class="dateButton">${getDateStringByDate(
-      date)}-${number}</button></td>
+                <td><button class="dateButton">${getDateStringByDate(date)}-${number}</button></td>
                 <td>${code}</td>
                 <td>${name}</td>
                 <td>${quantity}</td>
@@ -85,61 +69,52 @@ export const renderTable = (currentPage) => {
     tbody.appendChild(newTr);
   }
 
-  const dateButtons = document.querySelectorAll(".dateButton");
+  const dateButtons = document.querySelectorAll('.dateButton');
   dateButtons.forEach((row) => {
-    row.addEventListener("click", () => {
-      const parentRow = row.closest("tr");
-      const codeValue = parentRow.querySelectorAll("td")[2].innerText;
+    row.addEventListener('click', () => {
+      const parentRow = row.closest('tr');
+      const codeValue = parentRow.querySelectorAll('td')[2].innerText;
 
-      window.open(
-        `../add/sellAdd.html?mode=edit&code=${codeValue}`,
-        "_blank",
-        "width=1200,height=500"
-      );
+      window.open(`../add/sellAdd.html?mode=edit&code=${codeValue}`, '_blank', 'width=1200,height=500');
     });
   });
 };
 
-
 // 메인 로직
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   if (!localStorage.getItem(`sell_item_data`)) {
     localStorage.setItem(`sell_item_data`, JSON.stringify(sell_item_data));
   }
 
   // 최대 선택 날짜 오늘까지
-  const startDate = document.getElementById('startDate')
-  startDate.max = getDateStringByDate(new Date())
-  const endDate = document.getElementById('endDate')
-  endDate.max = getDateStringByDate(new Date())
+  const startDate = document.getElementById('startDate');
+  startDate.max = getDateStringByDate(new Date());
+  const endDate = document.getElementById('endDate');
+  endDate.max = getDateStringByDate(new Date());
 
   // startDate의 다음날을 endDate의 시작값으로 지정
   startDate.addEventListener('change', () => {
-    const endDate = document.getElementById('endDate')
-    const startDateValue = new Date(startDate.value)
-    const nextDay = new Date(startDateValue.getTime() + (24 * 60 * 60 * 1000));
-    endDate.min = getDateStringByDate(nextDay)
-  })
+    const endDate = document.getElementById('endDate');
+    const startDateValue = new Date(startDate.value);
+    const nextDay = new Date(startDateValue.getTime() + 24 * 60 * 60 * 1000);
+    endDate.min = getDateStringByDate(nextDay);
+  });
 
   // 1페이지 테이블 렌더링
   renderTable(currentPage);
 
-  const leftButton = document.querySelector(".leftButton");
-  const rightButton = document.querySelector(".rightButton");
+  const leftButton = document.querySelector('.leftButton');
+  const rightButton = document.querySelector('.rightButton');
 
-  leftButton.addEventListener("click", () => {
+  leftButton.addEventListener('click', () => {
     if (currentPage > 1) {
       currentPage -= 1; // 상태 업데이트
-      console.log(`currentPage2 ${currentPage}`);
       renderTable(currentPage);
     }
   });
 
-  rightButton.addEventListener("click", () => {
-    const totalPageCount =
-      totalCount % 10 === 0
-        ? Math.floor(totalCount / 10)
-        : Math.floor(totalCount / 10) + 1;
+  rightButton.addEventListener('click', () => {
+    const totalPageCount = totalCount % 10 === 0 ? Math.floor(totalCount / 10) : Math.floor(totalCount / 10) + 1;
     if (currentPage < totalPageCount) {
       currentPage += 1; // 상태 업데이트
 
@@ -166,10 +141,10 @@ document.addEventListener("DOMContentLoaded", () => {
   //   }
   // });
 
-  const searchButton = document.querySelector(".searchButton");
-  searchButton.addEventListener("click", () => {
-    if (document.querySelector(".codeList").innerHTML === "") {
-      alert("선택된 항목이 없습니다");
+  const searchButton = document.querySelector('.searchButton');
+  searchButton.addEventListener('click', () => {
+    if (document.querySelector('.codeList').innerHTML === '') {
+      alert('선택된 항목이 없습니다');
       return;
     }
 
@@ -179,23 +154,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const checkAll = document.getElementById("checkAll");
-checkAll.addEventListener("change", () => {
-  const allCheckboxes = document.querySelectorAll(".checkbox");
+const checkAll = document.getElementById('checkAll');
+checkAll.addEventListener('change', () => {
+  const allCheckboxes = document.querySelectorAll('.checkbox');
   allCheckboxes.forEach((checkbox) => {
     checkbox.checked = checkAll.checked ? true : false;
   });
 });
 
-const deleteButton = document.querySelector(".delete");
-deleteButton.addEventListener("click", () => {
-  const checkAll = document.getElementById("checkAll");
-  const allCheckboxes = document.querySelectorAll(".checkbox");
+const deleteButton = document.querySelector('.delete');
+deleteButton.addEventListener('click', () => {
+  const checkAll = document.getElementById('checkAll');
+  const allCheckboxes = document.querySelectorAll('.checkbox');
 
   allCheckboxes.forEach((checkbox) => {
     if (checkbox.checked) {
-      const targetTr = checkbox.closest("tr");
-      const codeValue = targetTr.querySelectorAll("td")[2].innerText;
+      const targetTr = checkbox.closest('tr');
+      const codeValue = targetTr.querySelectorAll('td')[2].innerText;
       deleteSellDataApi.delteRowByCode(codeValue);
     }
   });
@@ -204,22 +179,21 @@ deleteButton.addEventListener("click", () => {
   renderTable(currentPage);
 });
 
-const newButton = document.querySelector(".new");
-newButton.addEventListener("click", () => {
-  window.open(
-    `../add/sellAdd.html?mode=new`,
-    "_blank",
-    "width=1200,height=500"
-  );
+const newButton = document.querySelector('.new');
+newButton.addEventListener('click', () => {
+  window.open(`../add/sellAdd.html?mode=new`, '_blank', 'width=1200,height=500');
 });
 
-const searchProductDataButton = document.querySelector(
-  ".searchProductDataButton"
-);
-searchProductDataButton.addEventListener("click", () => {
-  window.open(
-    `../../../product/view/search/productSearch.html?from=search`,
-    "_blank",
-    "width=1200,height=500"
-  );
+const searchProductDataButton = document.querySelector('.searchProductDataButton');
+searchProductDataButton.addEventListener('click', () => {
+  window.open(`../../../product/view/search/productSearch.html?from=search`, '_blank', 'width=1200,height=500');
+});
+
+// 선택된 품목 삭제
+const codeList = document.querySelector('.codeList');
+codeList.addEventListener('click', (e) => {
+  const eventDrivenNode = e.target; // 이벤트 위임
+  if (eventDrivenNode.classList.contains('deleteSearchedData')) {
+    eventDrivenNode.closest('div').remove();
+  }
 });
